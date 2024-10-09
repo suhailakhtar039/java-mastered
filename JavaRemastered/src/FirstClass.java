9d332f51ba5bd495ac1192d841872cd658cd3248
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-09 19:17:17 +0530

    first class

---
+++ JavaRemastered/src/FirstClass.java
@@ -0,0 +1,5 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello there, I am going to learn java core or you can say java advanced");
+    }
+}

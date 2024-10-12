6b48c4bdbed7c9edb646c2ceee6564d067e2a412
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-12 23:41:37 +0530

    made changes in dog

--- JavaRemastered/src/Inheritance/Dog.java
+++ JavaRemastered/src/Inheritance/Dog.java
@@ -1,9 +1,30 @@
 package Inheritance;
 
-public class Dog extends Animal{
+public class Dog extends Animal {
 
-    Dog(){
+    private String earShape;
+    private String tailShape;
+
+    Dog() {
         super("Mutt", "XL", 200);
     }
 
+    public Dog(String type, double weight) {
+        this(type, weight, "perky", "curled");
+    }
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
+    }
 }

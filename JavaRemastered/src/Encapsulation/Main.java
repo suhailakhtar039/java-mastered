7f72ab4ce1f16505840bbe58b663cd519c1d9a03
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-14 13:03:09 +0530

    enhanced player used in main

--- JavaRemastered/src/Encapsulation/Main.java
+++ JavaRemastered/src/Encapsulation/Main.java
@@ -3,16 +3,19 @@ package Encapsulation;
 public class Main {
     public static void main(String[] args) {
 
-        Player player = new Player();
+        // Player player = new Player();
+        //
+        // player.name = "suhail";
+        // player.health = 200;
+        // player.weapon = "sword";
+        //
+        // int damage = 10;
+        //
+        // player.loseHealth(damage);
+        // System.out.println("health = " + player.healthRemaining());
 
-        player.name = "suhail";
-        player.health = 200;
-        player.weapon = "sword";
-
-        int damage = 10;
-
-        player.loseHealth(damage);
-        System.out.println("health = " + player.healthRemaining());
+        EnhancedPlayer enhancedPlayer = new EnhancedPlayer("suhail", 200, "sword");
+        System.out.println("Initial health is " + enhancedPlayer.healthRemaining());
 
     }
 }

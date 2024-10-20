c1eea10d185a6fd033fb648afbead3d0bc44cfe2
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-20 23:44:14 +0530

    expanding lambda expression

--- JavaRemastered/src/LambdaInterfaces/Lambda.java
+++ JavaRemastered/src/LambdaInterfaces/Lambda.java
@@ -18,5 +18,14 @@ public class Lambda {
             char first = myString.charAt(0);
             System.out.println(myString + " means " + first);
         });
+
+        int result = Calculator((a,b) -> a - b, 5, 2);
+
+
+    }
+    public static <T> T Calculator(Operation<T> function, T value1, T value2){
+        T result = function.operate(value1, value2);
+        System.out.println("Result of operation = " + result);
+        return result;
     }
 }

9c927b5ab9999b28e7a534ac3446dc9b6c2338f6
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-21 13:20:21 +0530

    binary operator created

--- JavaRemastered/src/LambdaInterfaces/Lambda.java
+++ JavaRemastered/src/LambdaInterfaces/Lambda.java
@@ -1,7 +1,10 @@
 package LambdaInterfaces;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.function.BinaryOperator;
 
 public class Lambda {
     public static void main(String[] args) {
@@ -19,12 +22,20 @@ public class Lambda {
             System.out.println(myString + " means " + first);
         });
 
-        int result = Calculator((a,b) -> a - b, 5, 2);
+        int result1 = Calculator((a, b) -> a - b, 5, 2);
+        String result2 = Calculator((a, b) -> a.toUpperCase(Locale.ROOT) + " " + b.toUpperCase(), "Suhail", "Akhtar");
 
+        List<double[]> coordinates = Arrays.asList(
+                new double[]{2, 4.08},
+                new double[]{23.1, 53.9},
+                new double[]{-34.1, -4.230});
+
+        coordinates.forEach(s -> System.out.println(Arrays.toString(s)));
 
     }
-    public static <T> T Calculator(Operation<T> function, T value1, T value2){
-        T result = function.operate(value1, value2);
+
+    public static <T> T Calculator(BinaryOperator<T> function, T value1, T value2) {
+        T result = function.apply(value1, value2);
         System.out.println("Result of operation = " + result);
         return result;
     }

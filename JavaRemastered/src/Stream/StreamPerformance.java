51201c7baa3231eac1fcc74c02f88e599885ac6f
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-22 12:00:03 +0530

    starting stream

---
+++ JavaRemastered/src/Stream/StreamPerformance.java
@@ -0,0 +1,11 @@
+package Stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamPerformance {
+    public static void main(String[] args) {
+        List<String> bingoPool = new ArrayList<>(75);
+        int start = 1;
+    }
+}

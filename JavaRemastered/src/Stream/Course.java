89e75ba2b9690bc9ef7b9d8f66e2f12be4ddca5a
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-23 23:52:58 +0530

    student engagement started

---
+++ JavaRemastered/src/Stream/Course.java
@@ -0,0 +1,18 @@
+package Stream;
+
+public record Course(String courseCode, String title, int lectureCount) {
+    public Course {
+        if (lectureCount <= 0) {
+            lectureCount = 1;
+        }
+    }
+
+    public Course(String courseCode, String title) {
+        this(courseCode, title, 40);
+    }
+
+    @Override
+    public String toString() {
+        return "%s %s".formatted(courseCode, title);
+    }
+}
---
+++ JavaRemastered/src/Stream/StudentEngagementStatistics.java
@@ -0,0 +1,7 @@
+package Stream;
+
+public class StudentEngagementStatistics {
+    public static void main(String[] args) {
+
+    }
+}

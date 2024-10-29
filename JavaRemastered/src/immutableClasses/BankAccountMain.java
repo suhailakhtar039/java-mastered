41dc1f935b46492940763e39a82c12e7cb3a68fc
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-29 15:16:20 +0530

    bank customer checked in bank account

--- JavaRemastered/src/immutableClasses/BankAccountMain.java
+++ JavaRemastered/src/immutableClasses/BankAccountMain.java
@@ -2,7 +2,10 @@ package immutableClasses;
 
 public class BankAccountMain {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount(BankAccount.AccountType.CHECKING, 500);
-        System.out.println(bankAccount);
+        // BankAccount bankAccount = new BankAccount(BankAccount.AccountType.CHECKING, 500);
+        // System.out.println(bankAccount);
+
+        BankCustomer bankCustomer = new BankCustomer("Suhail", 234.98, 1000);
+        System.out.println(bankCustomer);
     }
 }

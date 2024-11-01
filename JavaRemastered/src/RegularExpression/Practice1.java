f881ca791df12a38cbf6f999d8f96b31a57f7bc4
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-11-01 19:27:10 +0530

    selecting all lower and upper case along with digits

--- JavaRemastered/src/RegularExpression/Practice1.java
+++ JavaRemastered/src/RegularExpression/Practice1.java
@@ -9,9 +9,9 @@ public class Practice1 {
         String testString = "Anyone can Learn abc's, 123's and any regular expression";
         String replacement = "(-)";
         String[] patterns = {
-                "ab|bc",
-                "[123]",
-                "[A]"
+                "[a-z]",
+                "[0-9]",
+                "[A-Z]"
         };
 
         for (String pattern : patterns) {

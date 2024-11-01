f03f1be259fbd87bcb8adabe44df6c1d31a719a1
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-11-01 19:24:35 +0530

    using square brackets for regex

--- JavaRemastered/src/RegularExpression/Practice1.java
+++ JavaRemastered/src/RegularExpression/Practice1.java
@@ -9,14 +9,18 @@ public class Practice1 {
         String testString = "Anyone can Learn abc's, 123's and any regular expression";
         String replacement = "(-)";
         String[] patterns = {
-                "abc",
-                "123",
-                "A"
+                "[abc]",
+                "[123]",
+                "[A]"
         };
+
         for (String pattern : patterns) {
             String output = testString.replaceFirst(pattern, replacement);
             System.out.println("Pattern: " + pattern + " => " + output);
         }
+
+
+
     }
 
     private static String format(String regexp, String... args) {

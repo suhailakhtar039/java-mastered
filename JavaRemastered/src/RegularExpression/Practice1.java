b290530d7a65fc26c918e1dd757a38f1d24eb985
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-11-01 19:06:40 +0530

    regular expression practice

---
+++ JavaRemastered/src/RegularExpression/Practice1.java
@@ -0,0 +1,16 @@
+package RegularExpression;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        String s = "Hello world";
+        String ans = Practice1.format("%s %s", "Hello", "World!");
+        System.out.println(ans);
+    }
+    private static String format(String regexp, String ...args){
+        int index = 0;
+        while(regexp.contains("%s")){
+            regexp = regexp.replaceFirst("%s", args[index++]);
+        }
+        return regexp;
+    }
+}

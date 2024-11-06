7ac0aa882fccd67223e66a18dc1745a8ddefba75
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-11-06 13:26:54 +0530

    got the name of the current thread

---
+++ JavaRemastered/src/Thread/Example.java
@@ -0,0 +1,8 @@
+package Thread;
+
+public class Example {
+    public static void main(String[] args) {
+        Thread thread = Thread.currentThread();
+        System.out.println(thread.getName());
+    }
+}

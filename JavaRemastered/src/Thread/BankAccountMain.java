f7a54a4bd87b2271a76cdea5872c7983bc1f5f7c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-11-07 15:58:08 +0530

    bank account main filled with some value

--- JavaRemastered/src/Thread/BankAccountMain.java
+++ JavaRemastered/src/Thread/BankAccountMain.java
@@ -2,6 +2,14 @@ package Thread;
 
 public class BankAccountMain {
     public static void main(String[] args) {
+        BankAccount companyAccount = new BankAccount(10000);
 
+        Thread thread1 = new Thread(()->companyAccount.withdraw(2500));
+        Thread thread2 = new Thread(()->companyAccount.deposit(5000));
+        Thread thread3 = new Thread(()->companyAccount.withdraw(2500));
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }

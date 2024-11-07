774a952c36a05ea0715ea99dc5ed27ca1d6c0ace
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-11-07 16:40:25 +0530

    used synchronized block

--- JavaRemastered/src/Thread/BankAccount.java
+++ JavaRemastered/src/Thread/BankAccount.java
@@ -12,32 +12,32 @@ public class BankAccount {
         return balance;
     }
 
-    public synchronized void deposit(double amount){
-        try{
+    public void deposit(double amount) {
+        try {
             Thread.sleep(500);
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        double origBalance = balance;
-        this.balance += amount;
-        System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f): NEW BALANCE = %.0f%n", origBalance, amount, balance);
+        synchronized (this) {
+            double origBalance = balance;
+            this.balance += amount;
+            System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f): NEW BALANCE = %.0f%n", origBalance, amount, balance);
+        }
 
     }
 
-    public synchronized void withdraw(double amount){
-        try{
+    public synchronized void withdraw(double amount) {
+        try {
             Thread.sleep(500);
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         double origBalance = balance;
-        if(balance >= amount){
+        if (balance >= amount) {
             this.balance -= amount;
             System.out.printf("STARTING BALANCE: %.0f, WITHDRAWAL (%.0f): NEW BALANCE = %.0f%n", origBalance, amount, balance);
-        }
-        else{
+        } else {
             System.out.printf("STARTING BALANCE: %.0f, WITHDRAWAL (%.0f): INSUFFICIENT FUND", origBalance, amount);
         }
 

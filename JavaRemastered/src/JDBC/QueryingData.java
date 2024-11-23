7bdc6302ac8590aeffc4386c4fff51159ca3033c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-11-23 22:21:25 +0530

    music properties and querying data started

--- JavaRemastered/src/JDBC/QueryingData.java
+++ JavaRemastered/src/JDBC/QueryingData.java
@@ -1,7 +1,22 @@
 package JDBC;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Properties;
+
 public class QueryingData {
     public static void main(String[] args) {
+
+        Properties props = new Properties();
+        try{
+            props.load(Files.newInputStream(Path.of("music.properties"), StandardOpenOption.READ));
+
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
 
     }
 }

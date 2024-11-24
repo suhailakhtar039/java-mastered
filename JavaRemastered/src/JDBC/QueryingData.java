6b17f2001f322d52f0f1c913d8e9f061ba33eb2e
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-11-24 13:05:40 +0530

    meta data used for column information

--- JavaRemastered/src/JDBC/QueryingData.java
+++ JavaRemastered/src/JDBC/QueryingData.java
@@ -6,10 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Properties;
 
 public class QueryingData {
@@ -17,7 +14,7 @@ public class QueryingData {
 
         Properties props = new Properties();
         try {
-            props.load(Files.newInputStream(Path.of("C:\\Users\\Suhail Akhtar\\OneDrive\\Desktop\\project\\Java remastered\\JavaRemastered\\src\\music.properties"),StandardOpenOption.READ));
+            props.load(Files.newInputStream(Path.of("C:\\Users\\Suhail Akhtar\\OneDrive\\Desktop\\project\\Java remastered\\JavaRemastered\\src\\music.properties"), StandardOpenOption.READ));
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -35,8 +32,13 @@ public class QueryingData {
              Statement statement = connection.createStatement();
         ) {
             ResultSet resultSet = statement.executeQuery(query);
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                System.out.printf("%d %s %s %n", i, metaData.getColumnName(i), metaData.getColumnTypeName(i));
+            }
+            System.out.println("=========================");
 
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 System.out.printf("%d %s %s %n", resultSet.getInt("track_number"), resultSet.getString("artist_name"), resultSet.getString("song_title"));
             }
 

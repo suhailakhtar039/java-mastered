8b20fbfba87e3c3658ce20ae84421944a6ff489b
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-11-27 18:24:43 +0530

    connection made between application and SQL

--- JavaRemastered/src/JDBC/PreparedStatement.java
+++ JavaRemastered/src/JDBC/PreparedStatement.java
@@ -1,7 +1,27 @@
 package JDBC;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class PreparedStatement {
     public static void main(String[] args) {
+        MysqlDataSource dataSource = new MysqlDataSource();
 
+        dataSource.setServerName("localhost");
+        dataSource.setPort(3306);
+        dataSource.setDatabaseName("music");
+        try {
+            dataSource.setContinueBatchOnError(false);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try(Connection connection = dataSource.getConnection("devuser", System.getenv("MYSQL_PASS"))){
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }

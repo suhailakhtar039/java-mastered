8749612071c7cc43775bd3e007bbdb19942e3aac
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-02 13:13:53 +0530

    modified classpath correctly

--- JavaRemastered/src/JPA/Main.java
+++ JavaRemastered/src/JPA/Main.java
@@ -1,7 +1,26 @@
 package JPA;
 
+import JPA.music.Artist;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
+
+        try(EntityManagerFactory sessionFactory = Persistence.createEntityManagerFactory("JPA.music");
+            EntityManager entityManager = sessionFactory.createEntityManager();
+        ){
+
+            EntityTransaction transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(new Artist("Muddy Water"));
+            transaction.commit();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
     }
 }

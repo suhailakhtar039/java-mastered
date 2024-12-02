d5548ee2bf2e2f837059ee9ce6531056144b8a33
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-02 12:37:00 +0530

    modified class path of main

---
+++ JavaRemastered/src/JPA/Main.java
@@ -0,0 +1,7 @@
+package JPA;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- JavaRemastered/src/JPA/music/Artist.java
+++ JavaRemastered/src/JPA/music/Artist.java
@@ -1,4 +1,4 @@
-package JPA;
+package JPA.music;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;

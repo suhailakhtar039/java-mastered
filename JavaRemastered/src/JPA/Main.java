e273d3731d412173d85dad2acd8611c5a14ab328
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-02 22:37:29 +0530

    checking new album

--- JavaRemastered/src/JPA/Main.java
+++ JavaRemastered/src/JPA/Main.java
@@ -16,7 +16,9 @@ public class Main {
             EntityTransaction transaction = entityManager.getTransaction();
             transaction.begin();
             Artist artist = entityManager.find(Artist.class, 202);
-            artist.setArtistName("Muddy WaterS");
+            // artist.setArtistName("Muddy WaterS");
+            System.out.println(artist);
+            artist.addAlbum("The best of Suhail");
             System.out.println(artist);
             // entityManager.remove(artist);
             // entityManager.persist(new Artist("Muddy Water"));

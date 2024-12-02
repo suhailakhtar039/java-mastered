84d0b96044b7b94e3d4078034c6b33ed1e48525c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-02 12:09:09 +0530

    structure created

--- JavaRemastered/src/JPA/Artist.java
+++ JavaRemastered/src/JPA/Artist.java
@@ -1,7 +1,17 @@
 package JPA;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "artists")
 public class Artist {
+    @Id
+    @Column(name = "artist_id")
+    private int artistId;
+
+    @Column(name = "artist_name")
+    private String artistName;
 }

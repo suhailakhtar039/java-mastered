7c84b00cf6ab1b0472c4761f2058b7fb0407032b
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-02 23:05:03 +0530

    structure for main query created

--- JavaRemastered/src/JPA/MainQuery.java
+++ JavaRemastered/src/JPA/MainQuery.java
@@ -1,7 +1,21 @@
 package JPA;
 
+import JPA.music.Artist;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
 public class MainQuery {
     public static void main(String[] args) {
 
     }
+
+    private static List<Artist> getArtistsJPQL(EntityManager em, String matchedValue){
+        String jpql = "SELECT a FROM Artists a";
+        TypedQuery<Artist> query = em.createQuery(jpql, Artist.class);
+        return query.getResultList();
+    }
+
+
 }

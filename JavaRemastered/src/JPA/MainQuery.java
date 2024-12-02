b5208d1c57e0c7b7e5af1d7b948f29e5dd7e708b
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-02 23:26:30 +0530

    main query working using parameter

--- JavaRemastered/src/JPA/MainQuery.java
+++ JavaRemastered/src/JPA/MainQuery.java
@@ -8,20 +8,20 @@ import java.util.List;
 public class MainQuery {
     public static void main(String[] args) {
         List<Artist> artists = null;
-        try(EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA.music");
-            EntityManager em = emf.createEntityManager();
-        ){
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA.music");
+             EntityManager em = emf.createEntityManager();
+        ) {
             EntityTransaction transaction = em.getTransaction();
             transaction.begin();
             artists = getArtistsJPQL(em, "%Stev%");
             artists.forEach(System.out::println);
             transaction.commit();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static List<Artist> getArtistsJPQL(EntityManager em, String matchedValue){
+    private static List<Artist> getArtistsJPQL(EntityManager em, String matchedValue) {
         String jpql = "SELECT a FROM Artist a WHERE a.artistName LIKE :partialName";
         TypedQuery<Artist> query = em.createQuery(jpql, Artist.class);
         query.setParameter("partialName", matchedValue);

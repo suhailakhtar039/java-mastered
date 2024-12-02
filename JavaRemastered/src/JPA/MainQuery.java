4b61312d89fdfd54defd69ffd02c8a4ca74a36ff
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-02 23:15:55 +0530

    JPQL used in main

--- JavaRemastered/src/JPA/MainQuery.java
+++ JavaRemastered/src/JPA/MainQuery.java
@@ -1,18 +1,28 @@
 package JPA;
 
 import JPA.music.Artist;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
+import jakarta.persistence.*;
 
 import java.util.List;
 
 public class MainQuery {
     public static void main(String[] args) {
-
+        List<Artist> artists = null;
+        try(EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA.music");
+            EntityManager em = emf.createEntityManager();
+        ){
+            EntityTransaction transaction = em.getTransaction();
+            transaction.begin();
+            artists = getArtistsJPQL(em, "");
+            artists.forEach(System.out::println);
+            transaction.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     private static List<Artist> getArtistsJPQL(EntityManager em, String matchedValue){
-        String jpql = "SELECT a FROM Artists a";
+        String jpql = "SELECT a FROM Artist a";
         TypedQuery<Artist> query = em.createQuery(jpql, Artist.class);
         return query.getResultList();
     }

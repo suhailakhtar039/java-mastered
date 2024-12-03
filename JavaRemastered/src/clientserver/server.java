1a805bd7791eab39d7e87d7ea2e7351a580fbddc
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-03 22:47:48 +0530

    server file boilerplate created

--- JavaRemastered/src/clientserver/server.java
+++ JavaRemastered/src/clientserver/server.java
@@ -1,7 +1,31 @@
 package clientserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class server {
     public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
 
+            try (Socket socket = serverSocket.accept();) {
+                System.out.println("Server accepts client connection");
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+                while (true) {
+                    String echoString = input.readLine();
+                    System.out.println("server got request data: " + echoString);
+                    if (echoString.equals("exit")) break;
+                    output.println("echo from server: " + echoString);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("server exception " + e.getMessage());
+        }
     }
 }

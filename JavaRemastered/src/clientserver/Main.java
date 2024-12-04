def29ab6a23f9f522eba17f40a332406f8c94b05
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-04 16:09:43 +0530

    consumer function created

--- JavaRemastered/src/clientserver/Main.java
+++ JavaRemastered/src/clientserver/Main.java
@@ -1,11 +1,19 @@
 package clientserver;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
+        Consumer<ByteBuffer> printBuffer = buffer -> {
+            byte[] data = new byte[buffer.limit()];
+            buffer.get(data);
+            System.out.printf("\"%s\"", new String(data, StandardCharsets.UTF_8));
+        };
 
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        doOperation();
     }
 
     private static void doOperation(String op, ByteBuffer buffer, Consumer<ByteBuffer> c) {

14dbd4cf15b4af5852ca63ecb61f1440c8af5e7a
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-04 13:17:41 +0530

    server channel binding

--- JavaRemastered/src/clientserver/server/SimpleServerChannel.java
+++ JavaRemastered/src/clientserver/server/SimpleServerChannel.java
@@ -1,7 +1,19 @@
 package clientserver.server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+
 public class SimpleServerChannel {
     public static void main(String[] args) {
+
+        try(ServerSocketChannel serverChannel = ServerSocketChannel.open()){
+            serverChannel.socket().bind(new InetSocketAddress(5000));
+
+            System.out.println("Server is listening on port " + serverChannel.socket().getLocalPort());
+        }catch (IOException e){
+            System.out.println("Server Exception " + e.getMessage());
+        }
 
     }
 }

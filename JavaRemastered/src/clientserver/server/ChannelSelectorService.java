4c4694d9f483d7cf363901fe0d34071758bb1688
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-09 10:42:27 +0530

    structure created

--- JavaRemastered/src/clientserver/server/ChannelSelectorService.java
+++ JavaRemastered/src/clientserver/server/ChannelSelectorService.java
@@ -1,7 +1,16 @@
 package clientserver.server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+
 public class ChannelSelectorService {
     public static void main(String[] args) {
-
+        try(ServerSocketChannel serverChannel = ServerSocketChannel.open()){
+            serverChannel.bind(new InetSocketAddress(5000));
+            serverChannel.configureBlocking(false);
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }

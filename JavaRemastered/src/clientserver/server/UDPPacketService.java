a11b09fadea650f065cce32637474b99efa7a844
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-09 11:55:09 +0530

    UDP server structure created

--- JavaRemastered/src/clientserver/server/UDPPacketService.java
+++ JavaRemastered/src/clientserver/server/UDPPacketService.java
@@ -1,7 +1,29 @@
 package clientserver.server;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class UDPPacketService {
+
+    private static final int PORT = 5000;
+    private static final int PACKET_SIZE = 1024;
+
     public static void main(String[] args) {
+
+        try (DatagramSocket serverSocket = new DatagramSocket(PORT)) {
+
+            byte[] buffer = new byte[PACKET_SIZE];
+
+            System.out.println("Waiting for client to connect...");
+            DatagramPacket clientPacket = new DatagramPacket(buffer, buffer.length);
+            serverSocket.receive(clientPacket);
+            String audioFileName = new String(buffer, 0, clientPacket.getLength());
+            System.out.println("Client requested to listen to: " + audioFileName);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }

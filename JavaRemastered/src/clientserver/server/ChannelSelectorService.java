e1f217b213c41d1ddad5ea049a93b15569a56632
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-09 10:46:31 +0530

    created till selector

--- JavaRemastered/src/clientserver/server/ChannelSelectorService.java
+++ JavaRemastered/src/clientserver/server/ChannelSelectorService.java
@@ -2,13 +2,23 @@ package clientserver.server;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 
 public class ChannelSelectorService {
     public static void main(String[] args) {
         try(ServerSocketChannel serverChannel = ServerSocketChannel.open()){
+
             serverChannel.bind(new InetSocketAddress(5000));
             serverChannel.configureBlocking(false);
+            Selector selector = Selector.open();
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            while(true){
+                selector.select();
+            }
+
         }catch (IOException e){
             System.out.println(e.getMessage());
         }

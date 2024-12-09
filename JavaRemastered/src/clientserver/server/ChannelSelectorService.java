20db3382e02ea7366d3a6aad223ef55eeaae30e9
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-09 10:52:01 +0530

    channel selector service

--- JavaRemastered/src/clientserver/server/ChannelSelectorService.java
+++ JavaRemastered/src/clientserver/server/ChannelSelectorService.java
@@ -5,21 +5,39 @@ import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class ChannelSelectorService {
     public static void main(String[] args) {
-        try(ServerSocketChannel serverChannel = ServerSocketChannel.open()){
+        try (ServerSocketChannel serverChannel = ServerSocketChannel.open()) {
 
             serverChannel.bind(new InetSocketAddress(5000));
             serverChannel.configureBlocking(false);
             Selector selector = Selector.open();
             serverChannel.register(selector, SelectionKey.OP_ACCEPT);
 
-            while(true){
+            while (true) {
                 selector.select();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectedKeys.iterator();
+
+                while (iterator.hasNext()) {
+
+                    SelectionKey key = iterator.next();
+                    iterator.remove();
+
+                    if (key.isAcceptable()) {
+                        SocketChannel clientChannel = serverChannel.accept();
+                        System.out.println("Client connected: " + clientChannel.getRemoteAddress());
+                    }
+
+                }
+
             }
 
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }

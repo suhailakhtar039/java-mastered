d3062712bddd083057b97ec9a8a65d520df00231
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-10 13:31:13 +0530

    setting all the property

--- JavaRemastered/src/HttpBasics/HttpExample.java
+++ JavaRemastered/src/HttpBasics/HttpExample.java
@@ -1,13 +1,18 @@
 package HttpBasics;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpExample {
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.com");
-        }catch (IOException e){
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent","Chrome");
+            connection.setRequestProperty("Accept","application/json,text/html");
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }

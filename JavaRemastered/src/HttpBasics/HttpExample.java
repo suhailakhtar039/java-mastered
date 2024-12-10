9072cbd8699294fb9eecdddcc13fd00054bed618
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-10 13:25:07 +0530

    http create connection

--- JavaRemastered/src/HttpBasics/HttpExample.java
+++ JavaRemastered/src/HttpBasics/HttpExample.java
@@ -1,7 +1,14 @@
 package HttpBasics;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class HttpExample {
     public static void main(String[] args) {
-
+        try {
+            URL url = new URL("http://example.com");
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
 }

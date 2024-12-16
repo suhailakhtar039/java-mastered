7b77191a166565e86270b1907db59d9d37e6d29c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-16 19:13:25 +0530

    using bytes array

--- JavaRemastered/src/HttpBasics/SimpleHttpServer.java
+++ JavaRemastered/src/HttpBasics/SimpleHttpServer.java
@@ -5,6 +5,8 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+import static java.net.HttpURLConnection.HTTP_OK;
+
 public class SimpleHttpServer {
     public static void main(String[] args) {
         try {
@@ -14,7 +16,22 @@ public class SimpleHttpServer {
             server.createContext("/", exchange -> {
                 String requestMethod = exchange.getRequestMethod();
                 System.out.println("Request Method: " + requestMethod);
+
+                String response =
+                        """
+                                <html>
+                                    <body>
+                                        <h1>Hello World from my HTTP Server</h1>
+                                    </body>
+                                </html>
+                                """;
+
+                byte[] bytes = response.getBytes();
+                exchange.sendResponseHeaders(HTTP_OK, bytes.length);
+                exchange.getResponseBody().write(bytes);
+                exchange.close();
             });
+
 
             server.start();
             System.out.println("Server is listening on port 8080...");

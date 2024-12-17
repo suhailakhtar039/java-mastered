3fe1547415abe8e2688f826f1242b90b882a5f6e
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-17 16:24:17 +0530

    added visitor counter

--- JavaRemastered/src/HttpBasics/SimpleHttpServer.java
+++ JavaRemastered/src/HttpBasics/SimpleHttpServer.java
@@ -8,6 +8,8 @@ import java.net.InetSocketAddress;
 import static java.net.HttpURLConnection.HTTP_OK;
 
 public class SimpleHttpServer {
+
+    private static long visitorCounter = 0;
     public static void main(String[] args) {
         try {
 
@@ -17,14 +19,21 @@ public class SimpleHttpServer {
                 String requestMethod = exchange.getRequestMethod();
                 System.out.println("Request Method: " + requestMethod);
 
+                if(requestMethod.equals("POST")){
+                    visitorCounter++;
+                }
                 String response =
                         """
                                 <html>
                                     <body>
                                         <h1>Hello World from my HTTP Server</h1>
+                                        <p>Number of visitors who signed up = %d</p>
+                                        <form method="post">
+                                            <input type="submit" value="submit"/>
+                                        </form>
                                     </body>
                                 </html>
-                                """;
+                                """.formatted(visitorCounter);
 
                 byte[] bytes = response.getBytes();
                 exchange.sendResponseHeaders(HTTP_OK, bytes.length);

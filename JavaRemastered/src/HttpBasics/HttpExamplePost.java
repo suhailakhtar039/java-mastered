38fde9535c9ea2d9d94349dc07ca55a83f148144
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-17 16:35:29 +0530

    configuring changes

--- JavaRemastered/src/HttpBasics/HttpExamplePost.java
+++ JavaRemastered/src/HttpBasics/HttpExamplePost.java
@@ -12,7 +12,7 @@ import static java.net.HttpURLConnection.HTTP_OK;
 public class HttpExamplePost {
     public static void main(String[] args) {
         try {
-            
+
             // URL url = new URL("http://example.com");
             URL url = new URL("http://localhost:8080");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -20,10 +20,17 @@ public class HttpExamplePost {
             connection.setRequestProperty("User-Agent", "Chrome");
             connection.setRequestProperty("Accept", "application/json,text/html");
             connection.setReadTimeout(30000);
+
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            String parameters = "first=Joe&last=Smith";
+            int length = parameters.getBytes().length;
+            connection.setRequestProperty("Content-Length", String.valueOf(length));
+
             int responseCode = connection.getResponseCode();
             System.out.printf("Response code: %d%n", responseCode);
 
-            if(responseCode != HTTP_OK){
+            if (responseCode != HTTP_OK) {
                 System.out.println("Error reading web page " + url);
                 System.out.printf("Error: %s%n", connection.getResponseMessage());
                 return;

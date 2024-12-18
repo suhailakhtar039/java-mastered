f5d078d34c795313228491da0749c3d23076f8d6
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-18 13:24:52 +0530

    using post and creating URI

--- JavaRemastered/src/HttpBasics/HttpClientPost.java
+++ JavaRemastered/src/HttpBasics/HttpClientPost.java
@@ -1,8 +1,8 @@
 package HttpBasics;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -15,15 +15,15 @@ public class HttpClientPost {
         try {
 
             // URL url = new URL("http://example.com");
-            URL url = new URL("http://localhost:8080");
+            // URL url = new URL("http://localhost:8080");
             HttpClient client = HttpClient.newBuilder()
                     .connectTimeout(Duration.ofMinutes(1))
                     .version(HttpClient.Version.HTTP_1_1)
                     .build();
 
             HttpRequest request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(url.toURI())
+                    .POST(HttpRequest.BodyPublishers.ofString("first=suhail&last=akkhtar"))
+                    .uri(URI.create("http://localhost:8080"))
                     .header("User-Agent", "Chrome")
                     .headers("Accept", "application/json", "Accept", "text/html")
                     .timeout(Duration.ofSeconds(30))
@@ -31,12 +31,12 @@ public class HttpClientPost {
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             if (response.statusCode() != HTTP_OK) {
-                System.out.println("Error reading web page " + url);
+                System.out.println("Error reading web page " + request.uri());
                 return;
             }
             System.out.println(response.body());
 
-        } catch (IOException | URISyntaxException | InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }

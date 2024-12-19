d33084529a0442a9a4782e7666d28f1b49237b80
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-19 16:35:07 +0530

    demo class created

--- JavaRemastered/src/Debugger/Demo.java
+++ JavaRemastered/src/Debugger/Demo.java
@@ -1,4 +1,13 @@
 package Debugger;
 
 public class Demo {
+    public static void main(String[] args) {
+        StringUtilities utils = new StringUtilities();
+        StringBuilder sb = new StringBuilder();
+
+        while(sb.length()<10){
+            utils.addChar(sb, 'a');
+        }
+        System.out.println(sb);
+    }
 }

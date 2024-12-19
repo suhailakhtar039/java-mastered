14ee481aa4c9935134d3653f89cc8013b9fa1442
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-12-19 12:51:17 +0530

    implementing all the methods

--- JavaRemastered/src/websocket/SimpleWebSocketServer.java
+++ JavaRemastered/src/websocket/SimpleWebSocketServer.java
@@ -1,7 +1,33 @@
 package websocket;
 
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
 public class SimpleWebSocketServer extends WebSocketServer {
 
+    @Override
+    public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+
+    }
+
+    @Override
+    public void onClose(WebSocket webSocket, int i, String s, boolean b) {
+
+    }
+
+    @Override
+    public void onMessage(WebSocket webSocket, String s) {
+
+    }
+
+    @Override
+    public void onError(WebSocket webSocket, Exception e) {
+
+    }
+
+    @Override
+    public void onStart() {
+
+    }
 }
